8d2f0b9667c3f8791181d47e4e32e9662d1a3b13
Jaime Lopez <jaime.lopez@datainquiry.dev>
2025-09-05 10:51:05 -0400

    Moving boxing commented

--- src/movingbox.zig
+++ src/movingbox.zig
@@ -1,8 +1,30 @@
+/// This an example of a basic simulation setup.  The model is a entity
+/// (actually, a box) that is moving around the window. The mechanism is
+/// conformed by a step and a direction.  The window works as the context, that
+/// at the same time marks the frotier of the model.
+///
+/// To implement this example, the `raylib` library has been used.  If you want
+/// to compile directly this file, use the following command:
+///
+/// ```sh
+/// zig build-exe movingbox.zig -lraylib -lm -ldl -lpthread -lGL -lX11
+/// ```
+///
+/// Otherwise, if you have clone the repository, just run `zig build`.
+///
+/// There is a video in which this code was used as an example:
+///
+/// https://www.youtube.com/watch?v=KUcEGgAWxeE&t=33s
+///
+/// Copyright (c) 2025 Jaime Lopez - MIT License
+
 const std = @import("std");
 const rl = @cImport({
     @cInclude("raylib.h");
 });
 
+/// Pair represents a point in a bidimensional space. It is used as a brick to
+/// build composed shapes, like lines and rectangles.
 const Pair = struct {
     x: i32 = 0,
     y: i32 = 0,
@@ -46,12 +68,14 @@ const Rect = struct {
 };
 
 const Simulation = struct {
-    step: i32,
-    dim: i32,
-    win: Pair,
-    box: Rect,
-    direction: Pair,
+    step: i32,        // how far the box moves in each step
+    dim: i32,         // a size for the height and width of the box
+    win: Pair,        // Width and height of the window, the frontier
+    box: Rect,        // The like-agent in this model, a box
+    direction: Pair,  //  1 indicates moving from left to right, top to botton
+                      // -1 indicates moving from right to left, botton to top
 
+    /// The creation of the simulation and the setup of its initial conditions
     pub fn init(step: i32, dim: i32) Simulation {
         return .{
             .step = step,
@@ -65,23 +89,36 @@ const Simulation = struct {
         };
     }
 
+    /// The window acts as the frontier of the model. The box cannot go beyond
+    /// the window. When a border is reached by the box, the direction is
+    /// switched.
     fn updateDirection(self: *Simulation) void {
         if (self.box.pos.x + self.box.dim.x >= self.win.x)
+            // It reached the right border
             self.direction.x = -1
         else if (self.box.pos.x <= 0)
+            // It reached the left border
             self.direction.x = 1;
         if (self.box.pos.y + self.box.dim.y >= self.win.y)
+            // It reached the botton border
             self.direction.y = -1
         else if (self.box.pos.y <= 0)
+            // It reached the top border
             self.direction.y = 1;
     }
 
+    /// Updating the model. If the box has reached any of the borders,
+    /// directions are updated and a new positon for the box is assigned.
     pub fn update(self: *Simulation) void {
         self.updateDirection();
+        // x and y positions are updated
         self.box.pos.x += self.direction.x * self.step;
         self.box.pos.y += self.direction.y * self.step;
     }
 
+    /// Output for this model is a visualization. The box and a line from the
+    /// center of the window to center of the box are drawn in each step of the
+    /// simulation.
     pub fn draw(self: Simulation) void {
         const line = Line{
             .start = .{
@@ -96,10 +133,13 @@ const Simulation = struct {
 };
 
 pub fn main() void {
+    // Simulation initialization. It includes the low level stuff of creating
+    // the window and setting the frame rate.
     var sim = Simulation.init(10, 50);
     rl.InitWindow(sim.win.x, sim.win.y, "Moving Box");
     defer rl.CloseWindow();
     rl.SetTargetFPS(20);
+    // The loop in which the simulation evolves
     while (!rl.WindowShouldClose()) {
         rl.BeginDrawing();
         rl.ClearBackground(rl.BLACK);
