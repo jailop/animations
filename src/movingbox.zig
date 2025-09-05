96e4f87da2a7e42b7c18084a237c37c7b0404a5f
Jaime Lopez <jaime.lopez@datainquiry.dev>
2025-09-04 20:25:10 -0400

    Update

--- src/movingbox.zig
+++ src/movingbox.zig
@@ -11,59 +11,100 @@ const Pair = struct {
 const Line = struct {
     start: Pair = Pair{},
     end: Pair = Pair{},
+
     pub fn draw(self: Line) void {
         rl.DrawLine(
             self.start.x,
             self.start.y,
             self.end.x,
             self.end.y,
-            rl.DARKGRAY
+            rl.WHITE
         );
     }
 };
 
 const Rect = struct {
-    pos: Pair = Pair{},
-    dim: Pair = Pair{},
+    pos: Pair = Pair{},  // position
+    dim: Pair = Pair{},  // dimensions
+
     pub fn draw(self: Rect) void {
         rl.DrawRectangle(
             self.pos.x,
             self.pos.y,
             self.dim.x,
             self.dim.y,
-            rl.DARKGRAY
+            rl.WHITE
         );
+    }
+
+    pub fn center(self: Rect) Pair {
+        return .{
+            .x=self.pos.x + @divFloor(self.dim.x, 2),
+            .y=self.pos.y + @divFloor(self.dim.y, 2),
+        };
     }
 };
 
-const win = Pair{.x=640, .y=480};
+const Simulation = struct {
+    step: i32,
+    dim: i32,
+    win: Pair,
+    box: Rect,
+    direction: Pair,
+
+    pub fn init(step: i32, dim: i32) Simulation {
+        return .{
+            .step = step,
+            .dim = dim,
+            .win = .{.x = 640, .y = 480},
+            .box = .{
+                .pos = .{.x = 0, .y = 0},
+                .dim = .{.x = dim, .y = dim},
+            },
+            .direction = .{.x = 1, .y = 1},
+        };
+    }
+
+    fn updateDirection(self: *Simulation) void {
+        if (self.box.pos.x + self.box.dim.x >= self.win.x)
+            self.direction.x = -1
+        else if (self.box.pos.x <= 0)
+            self.direction.x = 1;
+        if (self.box.pos.y + self.box.dim.y >= self.win.y)
+            self.direction.y = -1
+        else if (self.box.pos.y <= 0)
+            self.direction.y = 1;
+    }
+
+    pub fn update(self: *Simulation) void {
+        self.updateDirection();
+        self.box.pos.x += self.direction.x * self.step;
+        self.box.pos.y += self.direction.y * self.step;
+    }
+
+    pub fn draw(self: Simulation) void {
+        const line = Line{
+            .start = .{
+                .x = @divFloor(self.win.x, 2),
+                .y = @divFloor(self.win.y, 2),
+            },
+            .end = self.box.center(),
+        };
+        line.draw();
+        self.box.draw();
+    }
+};
 
 pub fn main() void {
-    rl.InitWindow(win.x, win.y, "Moving Box");
-    rl.SetTargetFPS(120);
-    var x: i32 = 10;
-    var y: i32 = 10;
-    var xdir: i32 = 1;
-    var ydir: i32 = 1;
+    var sim = Simulation.init(10, 50);
+    rl.InitWindow(sim.win.x, sim.win.y, "Moving Box");
+    defer rl.CloseWindow();
+    rl.SetTargetFPS(20);
     while (!rl.WindowShouldClose()) {
         rl.BeginDrawing();
-        rl.ClearBackground(rl.RAYWHITE);
-        const box = Rect{
-            .pos=.{.x=x, .y=y},
-            .dim=.{.x=50, .y=50},
-        };
-        if (x > win.x - 60) xdir = -1;
-        if (x < 10) xdir = 1;
-        if (y > win.y - 60) ydir = -1;
-        if (y < 10) ydir = 1;
-        x += xdir * 10;
-        y += ydir * 10;
-        box.draw();
-        const line = Line{
-            .start=.{.x=win.x / 2, .y=win.y / 2},
-            .end=.{.x=x + 25, .y=y + 25}};
-        line.draw();
+        rl.ClearBackground(rl.BLACK);
+        sim.update();
+        sim.draw();
         rl.EndDrawing();
     }
-    defer rl.CloseWindow();
 }
